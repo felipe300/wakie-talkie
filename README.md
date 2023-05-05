20474edbf4f3a2bb42a813d531938a58f2306c50
Felipe300 <caracoloro@gmail.com>
2023-05-05 14:18:22 -0400

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# wakie-talkie
+# Wakie-talkie
+
+EJERCICIO:
+En la siguiente actividad crearás una simple aplicación en línea, la cual imita la forma en que funciona un
+walkie-talkie. Ésta tiene una interfaz que permite ingresar dos entradas, siendo la primera un mensaje, y la
+segunda el destinatario de dicho mensaje.
+
+![wakie](./assets/img/wakie-talkie.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log('testing wakie-talkie!')
+let btn = document.getElementById("btn")
+let isSent = false
+
+function displayMessage (info) {
+	document.getElementById("response").innerHTML = info
+}
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault()
+	let response = document.getElementById("response")
+
+	let promise = new Promise((resolve, reject) => {
+		if (!isSent) {
+			resolve(`Mensaje enviado`)
+			response.classList.add("isOk")
+			isSent = true
+		} else {
+			reject("Ya has enviado un mensaje. No puedes enviar otro mensaje.")
+			response.classList.add("isError")
+		}
+	})
+
+	promise
+		.then((message) => {
+			displayMessage(message)
+		})
+		.catch((err) => {
+			displayMessage(err)
+		})
+
+	setTimeout(() => {
+		response.innerHTML = ''
+		response.classList.remove("isOk")
+		response.classList.remove("isError")
+	}, 2000)
+})
